a1fbd7a08d02df6c46fd5aac8e04647e0ef516e6
menikhilpandey <menikhilpandey@gmail.com>
2020-10-04 16:00:32 +0530

    Wallet model created

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.db.models import (
+    FloatField,
+    CharField,
+    BooleanField,
+    DateTimeField,
+    Model,
+)
 
-# Create your models here.
+
+class Wallet(Model):
+    """ Wallet Model """
+    name = CharField(max_length=63)
+    wallet_type = CharField(
+        max_length=63,
+        default='liquid_assets'
+    )
+    currency = CharField(max_length=7)
+    balance = FloatField(default=0)
+    exclude_from_total = BooleanField(default=False)
+    isActive = BooleanField(default=True)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("name", "wallet_type")
+
+    def __str__(self):
+        return self.name + self.wallet_type + self.balance
